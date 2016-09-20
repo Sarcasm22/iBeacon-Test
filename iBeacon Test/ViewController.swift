55267024e7c5b5c20e1253803476fbd77cbd3707
Code Box <AJ@codes-macbook-air.local>
2016-09-20 13:45:57 -0400

    Initial Commit

---
+++ iBeacon Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeacon Test
+//
+//  Created by Code Box on 9/20/16.
+//  Copyright © 2016 Uncle Austin's Premium Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
